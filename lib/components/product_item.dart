853ba01067f04cf8de526ef3891b8e525b8b7a8e
And5reas <And5reas.porcel@hotmail.com>
2025-01-20 15:02:20 -0300

    Starting project

---
+++ lib/components/product_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem({
+    required this.product,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite),
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart),
+          ),
+        ),
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
