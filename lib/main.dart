595813121a51361a9e4416a2e9779bfa67bf064a
And5reas <And5reas.porcel@hotmail.com>
2025-01-20 16:39:04 -0300

    Using Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -13,27 +15,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shopping',
-      theme: ThemeData(
-        fontFamily: 'Lato',
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.deepPurple,
-            secondary: Colors.deepOrange,
-            primary: Colors.purple),
-        appBarTheme: AppBarTheme(
-          titleTextStyle: TextStyle(color: Colors.white, fontSize: 22),
-          iconTheme: IconThemeData(color: Colors.white),
-          backgroundColor: Colors.deepPurple,
-          centerTitle: true,
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Shopping',
+        theme: ThemeData(
+          fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.deepPurple,
+              secondary: Colors.deepOrange,
+              primary: Colors.purple),
+          appBarTheme: AppBarTheme(
+            titleTextStyle: TextStyle(color: Colors.white, fontSize: 22),
+            iconTheme: IconThemeData(color: Colors.white),
+            backgroundColor: Colors.deepPurple,
+            centerTitle: true,
+          ),
+          useMaterial3: true,
         ),
-        useMaterial3: true,
+        routes: {
+          AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
+        },
+        home: ProductsOverviewPage(),
+        debugShowCheckedModeBanner: false,
       ),
-      routes: {
-        AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
-      },
-      home: ProductsOverviewPage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
