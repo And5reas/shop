14016d2034d1dccb1dc825fba23000039476b9e4
And5reas <And5reas.porcel@hotmail.com>
2025-01-21 11:29:24 -0300

    Order system implemented

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/badgee.dart';
 import 'package:shop/components/product_grid.dart';
+import 'package:shop/components/app_drawer.dart';
 import 'package:shop/models/cart.dart';
 import 'package:shop/utils/app_routes.dart';
 
@@ -65,6 +66,7 @@ class _ProductsOverviewPageState extends State<ProductsOverviewPage> {
         ],
       ),
       body: ProductGrid(_showOnlyFavorites),
+      drawer: AppDrawer(),
     );
   }
 }
