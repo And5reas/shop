c0ea156d5fe406de777a0be3e0c28ff12d3aea67
And5reas <And5reas.porcel@hotmail.com>
2025-01-22 10:47:36 -0300

    Everthing in claud

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -7,20 +7,45 @@ import 'package:shop/models/order_list.dart';
 class OrdersPage extends StatelessWidget {
   const OrdersPage({super.key});
 
+  Future<void> _refreshOrders(BuildContext context) async {
+    return Provider.of<OrderList>(context, listen: false).loadOrders();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Meus Pedidos'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemCount: orders.itemsCount,
-        itemBuilder: (ctx, i) => OrderWidget(
-          order: orders.items[i],
-        ),
-      ),
+      // body: _isLoading
+      //     ?
+      //     :
+      body: FutureBuilder(
+          future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+          builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.error != null) {
+              return Center(
+                child: Text('Ocorreu um erro inesperado'),
+              );
+            } else {
+              return Consumer<OrderList>(
+                builder: (ctx, orders, child) => RefreshIndicator(
+                  onRefresh: () => _refreshOrders(context),
+                  child: ListView.builder(
+                    itemCount: orders.itemsCount,
+                    itemBuilder: (ctx, i) => OrderWidget(
+                      order: orders.items[i],
+                    ),
+                  ),
+                ),
+              );
+            }
+          }),
     );
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   static const productsBaseUrl =
       'https://shop-udemy-4285f-default-rtdb.firebaseio.com/products';
+  static const ordersBaseUrl =
+      'https://shop-udemy-4285f-default-rtdb.firebaseio.com/orders';
 }
